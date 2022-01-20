3101feee5b8c19763b22431bd0a9f1b367582db2
Gbenge Aondoakula Raphael <gbengeraphael@gmail.com>
2022-01-20 22:57:44 +0100

    List Screen init component

---
+++ src/screens/ListScreen.js
@@ -0,0 +1,19 @@
+//imports
+import React from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+
+//component
+const ListScreen = () => {
+
+
+    return (<View>
+        <Text >List Screen</Text>
+    </View>)
+}
+
+// styles 
+const style = StyleSheet.create({
+})
+
+/// export
+export default ListScreen
