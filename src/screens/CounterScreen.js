580cdd9f54ea9c62fab95c2a242d8b96ce3b4c63
Gbenge Aondoakula Raphael <gbengeraphael@gmail.com>
2022-01-21 22:12:44 +0100

    counter working

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,9 +1,19 @@
-import React from 'react'
-import { Text, FlatList, StyleSheet, View, Counter } from 'react-native'
+import React, { useState } from 'react'
+import { Text, FlatList, StyleSheet, View, Button } from 'react-native'
 
 const CounterScreen = () => {
+    const [counter, setCounter] = useState(0)
+    // don't assign to counter  but use setCounter function
     return <View>
-        <Text>Current Count</Text>
+        <Button title='Increase'
+            onPress={() => {
+                setCounter(counter + 1)
+            }} />
+        <Button title='Decrease'
+            onPress={() => {
+                setCounter(counter - 1)
+            }} />
+        <Text>Current Count: {counter}</Text>
     </View>
 }
 const styles = StyleSheet.create({
