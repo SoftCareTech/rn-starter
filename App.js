0919da597ad9f986c2d7f7dfa3bafc1940b4ede3
Gbenge Aondoakula Raphael <gbengeraphael@gmail.com>
2022-01-21 23:28:55 +0100

    State : Square App init

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import ImageScreen from "./src/screens/ImageScreen";
 import CounterScreen from "./src/screens/CounterScreen";
 import ColorScreen from "./src/screens/ColorScreen";
 
+import SquareScreen from "./src/screens/SquareScreen";
 
 
 const navigator = createStackNavigator(
@@ -16,10 +17,11 @@ const navigator = createStackNavigator(
     List: ListScreen,
     Image: ImageScreen,
     Counter: CounterScreen,
-    Color: ColorScreen
+    Color: ColorScreen,
+    Square: SquareScreen
   },
   {
-    initialRouteName: "Color",
+    initialRouteName: "Square",
     defaultNavigationOptions: {
       title: "App",
     },
---
+++ src/screens/SquareScreen.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+
+//Square
+const SquareScreen = () => {
+       return (<View>
+        <Text  >Square </Text>
+         
+    </View>)
+}
+
+// styles 
+const styles = StyleSheet.create({
+    
+})
+
+/// export
+export default SquareScreen
