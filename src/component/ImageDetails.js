a79d73a71fd1dfef05326ea9d6fe344d519214f8
Gbenge Aondoakula Raphael <gbengeraphael@gmail.com>
2022-01-21 21:43:49 +0100

    End of Image waiting for repose for web

--- src/component/ImageDetails.js
+++ src/component/ImageDetails.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import { Text, Image, StyleSheet, View } from 'react-native'
 
-const ImageDetail = props => {
+import imgSrcM from '../../assets/mountain.jpg';
+const ImageDetail = (props) => {
+    console.log(props)
+    // <Image source={props.imageSource} />
     return <View>
-        <Image source={require('../../assets/beach.jpg')}/>
+        <Image source={{ uri: props.imageSource }}
+            style={styles.image} />
         <Text> {props.title} </Text>
     </View>
 }
-const style = StyleSheet.create({
-
+const styles = StyleSheet.create({
+    image: {
+        width: '150px',
+        height: '150px',
+        borderWidth: 1
+    }
 })
 export default ImageDetail
--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -1,15 +1,29 @@
 import React from 'react'
-import { Text, FlatList, StyleSheet, View } from 'react-native'
+import { Text, FlatList, StyleSheet, View,Image } from 'react-native'
 import ImageDetail from "../component/ImageDetails";
 
+import imgSrcF from '../../assets/forest.jpg';
+import imgSrcB from '../../assets/beach.jpg';
+import imgSrcM from '../../assets/mountain.jpg';
+
 const ImageScreen = () => {
+    // imgageSource={require('../../assets/forest.jpg')} />
     return <View>
-        <ImageDetail title='Forest'/>
-        <ImageDetail title='Moutain'/>
-        <ImageDetail title='Beach'/>
+        <Text>Hello</Text>
+        <Image source={{ uri: imgSrcF }} style={style.image} />
+        <ImageDetail title='Forest'
+            imgageSource={imgSrcF} />
+        <ImageDetail title='Moutain'
+            imgageSource={imgSrcM} />
+        <ImageDetail title='Beach'
+            imgageSource={imgSrcB} />
     </View>
 }
 const style = StyleSheet.create({
-
+    image: {
+        width: 150,
+        height: 150,
+        borderWidth: 1
+    }
 })
 export default ImageScreen
