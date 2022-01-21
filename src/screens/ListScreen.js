ebbff085c924901387ab4c74b2b7bffcac1d2c11
Gbenge Aondoakula Raphael <gbengeraphael@gmail.com>
2022-01-21 09:53:09 +0100

    method 1 Adding key

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -5,15 +5,17 @@ import { Text, FlatList, StyleSheet, View } from 'react-native'
 //component
 const ListScreen = () => {
 const friends=[
-{name:'Friend 1'},
-{name:'Friend 2'},
-{name:'Friend 3'}
+{name:'Friend 1', key:'1'},
+{name:'Friend 2',key:'2'},
+{name:'Friend 3',key:'3'}
 
 ]
 
  
     
-   return <FlatList  data ={friends}
+   return <FlatList 
+       
+           data ={friends}
             renderItem={({item })=>{ 
                 return <Text>{item.name} </Text>
             }
