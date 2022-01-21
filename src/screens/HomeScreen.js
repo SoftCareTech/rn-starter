75c9a844c3a5eca0a5995d9e929609a35b1d4dc7
Gbenge Aondoakula Raphael <gbengeraphael@gmail.com>
2022-01-21 10:49:32 +0100

    Button and TouchableOpacity

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { View, Button, TouchableOpacity, Text, StyleSheet } from 'react-native';
 
- 
+
 const HomeScreen = () => {
-  return <Text style={styles.text}>Hi there... I am going to follow step by step no jumping </Text>;
-};
+  return <View>
+    <Text style={styles.text}>Hi there... I am going to follow step by step no jumping </Text>
+    <Button
+      title='Go to Component Demo'
+      onPress={() => {
+        console.log("Componet press")
+      }}
+    /> 
+    <TouchableOpacity 
+      onPress={() => {
+        console.log("list press")
+      }}>
+      <Text>Go to list Demo</Text>
+    </TouchableOpacity>
+
+  </View>
+}
 
 
 const styles = StyleSheet.create({
