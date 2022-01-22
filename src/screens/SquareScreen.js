f406955096f48820a6c80c8dc59023955ba9fe90
Gbenge Aondoakula Raphael <gbengeraphael@gmail.com>
2022-01-22 10:43:56 +0100

    Square color working

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -11,20 +11,34 @@ const SquareScreen = () => {
     console.log(red)
     console.log(green)
     console.log(blue)
+    const setColor = (color, change) => {
+        //color ==== red green blue
+        switch (color) {
+            case 'red':
+                red + change > 255 || red + change < 0 ? null : setRed(red + change)
+                return
+            case 'green':
+                green + change > 255 || green + change < 0 ? null : setGreen(green + change)
+                return
+            case 'blue':
+                blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change)
+                return
+        }
+    }
     return (<View>
         <ColorCounter
-            onDecrease={() => setRed(red - COLOR_INCREAMENT)}
-            onIncrease={() => setRed(red + COLOR_INCREAMENT)}
+            onDecrease={() => setColor('red', -COLOR_INCREAMENT)}
+            onIncrease={() => setColor('red', COLOR_INCREAMENT)}
             color={'Red'}
         />
         <ColorCounter
-            onDecrease={() => setGreen(green - COLOR_INCREAMENT)}
-            onIncrease={() => setGreen(green + COLOR_INCREAMENT)}
+            onDecrease={() => setColor('green ', - COLOR_INCREAMENT)}
+            onIncrease={() => setColor('green', COLOR_INCREAMENT)}
             color={'Green'}
         />
         <ColorCounter
-            onDecrease={() => setBlue(blue - COLOR_INCREAMENT)}
-            onIncrease={() => setBlue(blue + COLOR_INCREAMENT)}
+            onDecrease={() => setColor('blue', - COLOR_INCREAMENT)}
+            onIncrease={() => setColor('blue', COLOR_INCREAMENT)}
             color={'Blue'}
         />
 
