af78a530df2233060a352a295e2751d25ddb47b8
Gbenge Aondoakula Raphael <gbengeraphael@gmail.com>
2022-01-22 16:32:41 +0100

    TextInput done

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,16 +1,30 @@
- import React from 'react'
-import { Text, StyleSheet, View } from 'react-native'
+import React, { useState } from 'react'
+import { Text, StyleSheet, View, TextInput } from 'react-native'
 
 //component
 const TextScreen = () => {
-    const myName = 'Gbenge Aondoakula Raphael'
+    const [name, setName] = useState('')
+
     return (<View>
-          <Text  >Text Screen </Text>
+        <Text  >Text Screen </Text>
+        <TextInput
+            autoCapitalize='none'
+            autoCorrect={false}
+            style={styles.input}
+            value={name}
+            onChangeText={(newValue) => setName(newValue)}
+        />
+        <Text  >my name  is {name} </Text>
     </View>)
 }
 
 // styles 
-const styles = StyleSheet.create({ 
+const styles = StyleSheet.create({
+    input: {
+        margin: 15,
+        borderColor: 'black',
+        borderWidth: 1
+    }
 })
 
 /// export
