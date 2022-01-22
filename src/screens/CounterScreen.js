5663bc9b5183f4e4de7223491cb701116e2016e7
Gbenge Aondoakula Raphael <gbengeraphael@gmail.com>
2022-01-22 14:33:57 +0100

    Exercise  Solution  Modify to mentor solution

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,19 +1,29 @@
 import React, { useReducer } from 'react'
 import { Text, FlatList, StyleSheet, View, Button } from 'react-native'
-const reducer = (state, change) => {
+const reducer = (state, action) => {
+    // state == {count:number}
+    // action == {type:'increament||decreament', paload:1}
+    switch (action.type) {
+        case 'increament':
+            return { ...state, count: state.count + action.payload }
+        case 'decreament':
+            return { ...state, count: state.count - action.payload }
+        default:
+            return state
+    }
     return state + change
 }
 const CounterScreen = () => {
-    const [state, dispatch] = useReducer(reducer, 0)
+    const [state, dispatch] = useReducer(reducer, { count: 0 })
 
     return <View>
         <Button title='Increase'
-            onPress={() => dispatch(+ 1)}
+            onPress={() => dispatch({ type: 'increament', payload: 1 })}
         />
         <Button title='Decrease'
-            onPress={() => dispatch(- 1)}
+            onPress={() => dispatch({ type: 'decreament', payload: 1 })}
         />
-        <Text>Current Count: {state}</Text>
+        <Text>Current Count: {state.count}</Text>
 
     </View>
 }
