48f715d549b094f16a1cc18c0fbcab1c469349dd
Gbenge Aondoakula Raphael <gbengeraphael@gmail.com>
2022-01-22 17:02:22 +0100

    Layout:Box init

---
+++ src/screens/BoxScreen.js
@@ -0,0 +1,21 @@
+//imports
+import React from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+
+//Box
+const BoxScreen = () => {
+    return (<View>
+        <Text style={styles.textStyleSubHead} >Box Screen</Text>
+
+    </View>)
+}
+
+// styles 
+const styles = StyleSheet.create({
+    viewStyle: {
+
+    }
+})
+
+/// export
+export default BoxScreen
