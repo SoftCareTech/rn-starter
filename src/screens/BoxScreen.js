f1e51e0602fe0bae0246daf6de26703aedba371c
Gbenge Aondoakula Raphael <gbengeraphael@gmail.com>
2022-01-23 04:46:15 +0100

    Layout Exercise done

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -5,20 +5,48 @@ import { Text, StyleSheet, View } from 'react-native'
 //Box
 const BoxScreen = () => {
     return (<View style={styles.viewStyle}>
-        <Text style={styles.textStyle1} >Box 1</Text>
-        <Text style={styles.textStyle2} >Box 2</Text>
-        <Text style={styles.textStyle3} >Box 3</Text>
-
+        <View style={styles.view1} />
+        <View style={styles.view2_2} />
+        <View style={styles.view3} />
     </View>)
 }
 
+//Exercise to in two ways for midle view have two styles
+// that is styles.view2_1, styles.view2_2
 // styles 
 const styles = StyleSheet.create({
     viewStyle: {
         borderWidth: 1,
         borderColor: 'black',
         height: 234,
-
+        flexDirection: 'row'
+        , justifyContent: 'space-between'
+    },
+    view1: {
+        borderWidth: 1,
+        borderColor: 'black', backgroundColor: 'yellow',
+        height: 100,
+        width: 100
+    },
+    view2_1: {
+        borderWidth: 1,
+        borderColor: 'black', backgroundColor: 'pink',
+        height: 100,
+        width: 100,
+        top: 100
+    },
+    view2_2: {
+        borderWidth: 1,
+        borderColor: 'black', backgroundColor: 'pink',
+        height: 100,
+        width: 100,
+        marginTop: 100
+    },
+    view3: {
+        borderWidth: 1,
+        borderColor: 'black', backgroundColor: 'green',
+        height: 100,
+        width: 100
     },
     textStyle1: {
         borderWidth: 3,
